6479c8e1378bedf6d3494ec3df8cba108e782e0f
Valentin Karayvansky <Valik-isuzu@mail.ru>
2016-09-29 20:23:48 +0300

    Two tasks done

--- src/com/netcracker/edu/main/Main.java
+++ src/com/netcracker/edu/main/Main.java
@@ -3,6 +3,12 @@ package com.netcracker.edu.main;
 public class Main {
 
     public static void main(String[] args) {
-        //TODO something
+        print(args);
+    }
+
+    public static void print(String[] args) {
+        for (String line : args) {
+            System.out.println(line);
+        }
     }
 }
