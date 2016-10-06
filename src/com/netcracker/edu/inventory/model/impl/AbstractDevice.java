92ae0944fccccbfcff98613c9badf91d16f38fb7
Valentin Karayvansky <Valik-isuzu@mail.ru>
2016-10-06 23:33:49 +0300

    Task 3

---
+++ src/com/netcracker/edu/inventory/model/impl/AbstractDevice.java
@@ -0,0 +1,66 @@
+package com.netcracker.edu.inventory.model.impl;
+
+import com.netcracker.edu.inventory.model.Device;
+
+import java.util.Date;
+
+public abstract class AbstractDevice implements Device {
+
+    protected int in;
+    protected String type;
+    protected String manufacturer;
+    protected String model;
+    protected Date productionDate;
+
+    @Override
+    public int getIn() {
+        return in;
+    }
+
+    @Override
+    public void setIn(int in) {
+        this.in = in;
+    }
+
+    @Override
+    public String getType() {
+        return type;
+    }
+
+    @Override
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    @Override
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    @Override
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    @Override
+    public String getModel() {
+        return model;
+    }
+
+    @Override
+    public void setModel(String model) {
+        this.model = model;
+    }
+
+    @Override
+    public Date getProductionDate() {
+        return productionDate;
+    }
+
+    @Override
+    public void setProductionDate(Date productionDate) {
+        this.productionDate = productionDate;
+    }
+
+
+}
