5c82679fc322f0822266206ab7c41e0471b0b851
Valentin Karayvansky <Valik-isuzu@mail.ru>
2016-10-07 15:03:19 +0300

    All tasks done

--- src/com/netcracker/edu/inventory/model/impl/RackArrayImpl.java
+++ src/com/netcracker/edu/inventory/model/impl/RackArrayImpl.java
@@ -3,53 +3,83 @@ package com.netcracker.edu.inventory.model.impl;
 import com.netcracker.edu.inventory.model.Device;
 import com.netcracker.edu.inventory.model.Rack;
 
-public class RackArrayImpl  {
+public class RackArrayImpl implements Rack {
 
-//    private String type;
-//    private Device[] devices;
-//
-//    public RackArrayImpl(int size, String type) {
-//        this.type = type;
-//        if (type.equals("Battery")) {
-//            devices = new Battery[size];
-//        } else {
-//            System.err.println("Wrong type");
-//        }
-//    }
-//
-//    @Override
-//    public int getSize() {
-//
-//    }
-//
-//    @Override
-//    public int getFreeSize() {
-//    }
-//
-//    @Override
-//    public Device getDevAtSlot(int index) {
-//        return null;
-//    }
-//
-//    @Override
-//    public boolean insertDevToSlot(Device device, int index) {
-//        return false;
-//    }
-//
-//    @Override
-//    public Device removeDevFromSlot(int index) {
-//        return null;
-//    }
-//
-//    @Override
-//    public Device getDevByIN(int in) {
-//        return null;
-//    }
-//
-//}
-//
-//    public String getType() {
-//        return type;
-//    }
+    private String type;
+    private Device[] devices;
+
+    public RackArrayImpl(int size, String type) {
+        this.type = type;
+        devices = new Device[size];
+    }
+
+    @Override
+    public int getSize() {
+        return devices.length;
+    }
+
+    @Override
+    public int getFreeSize() {
+        int freeSlots = devices.length;
+        for (Device device : devices) {
+            if (device == null) {
+                freeSlots++;
+            }
+        }
+        return freeSlots;
+    }
+
+    @Override
+    public Device getDevAtSlot(int index) {
+        if (index >= getFreeSize() || index < 0) {
+            return null;
+        }
+        if (devices[index] == null) {
+            return null;
+        }
+        return devices[index];
+    }
+
+    @Override
+    public boolean insertDevToSlot(Device device, int index) {
+        if (index >= getSize() || index < 0) {
+            return false;
+        }
+        if (getDevAtSlot(index) != null) {
+            return false;
+        }
+        if (!type.equals(device.getType())) {
+            return false;
+        }
+        devices[index] = device;
+        return true;
+    }
+
+    @Override
+    public Device removeDevFromSlot(int index) {
+        if (index >= getFreeSize() || index < 0) {
+            return null;
+        }
+        if (devices[index] == null) {
+            return null;
+        }
+        Device device = devices[index];
+        devices[index] = null;
+        return device;
+    }
+
+    @Override
+    public Device getDevByIN(int in) {
+        for (Device device : devices) {
+            if (device != null && device.getIn() == in) {
+                return device;
+            }
+        }
+        return null;
+    }
+
+    public String getType() {
+        return type;
+    }
 
 }
--- src/com/netcracker/edu/inventory/service/impl/ServiceImpl.java
+++ src/com/netcracker/edu/inventory/service/impl/ServiceImpl.java
@@ -1,4 +1,17 @@
 package com.netcracker.edu.inventory.service.impl;
 
-public class ServiceImpl {
+import com.netcracker.edu.inventory.model.Device;
+import com.netcracker.edu.inventory.service.Service;
+
+public class ServiceImpl implements Service {
+
+    @Override
+    public void sortByIN(Device[] devices) {
+        System.err.println("Operation sortByIN not supported yet");
+    }
+
+    @Override
+    public void filtrateByType(Device[] devices, String type) {
+        System.err.println("Operation filtrateByType not supported yet");
+    }
 }
