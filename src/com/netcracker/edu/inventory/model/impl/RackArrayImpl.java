807e88e9b4822e62ce9a299031181174b50bfd95
Valentin Karayvansky <Valik-isuzu@mail.ru>
2016-10-07 09:46:09 +0300

    Work on 5 and 6 tasks

--- src/com/netcracker/edu/inventory/model/impl/RackArrayImpl.java
+++ src/com/netcracker/edu/inventory/model/impl/RackArrayImpl.java
@@ -3,54 +3,53 @@ package com.netcracker.edu.inventory.model.impl;
 import com.netcracker.edu.inventory.model.Device;
 import com.netcracker.edu.inventory.model.Rack;
 
-public class RackArrayImpl implements Rack {
+public class RackArrayImpl  {
 
-    private String type;
-    private Device[] devices;
-
-    public RackArrayImpl(int size, String type) {
-        this.type = type;
-        if (type.equals("Battery")) {
-            devices = new Battery[size];
-
-        } else {
-            System.err.println("Wrong type");
-        }
-    }
-
-    @Override
-    public int getSize() {
-
-    }
-
-    @Override
-    public int getFreeSize() {
-    }
-
-    @Override
-    public Device getDevAtSlot(int index) {
-        return null;
-    }
-
-    @Override
-    public boolean insertDevToSlot(Device device, int index) {
-        return false;
-    }
-
-    @Override
-    public Device removeDevFromSlot(int index) {
-        return null;
-    }
-
-    @Override
-    public Device getDevByIN(int in) {
-        return null;
-    }
-
-}
-
-    public String getType() {
-        return type;
-    }
+//    private String type;
+//    private Device[] devices;
+//
+//    public RackArrayImpl(int size, String type) {
+//        this.type = type;
+//        if (type.equals("Battery")) {
+//            devices = new Battery[size];
+//        } else {
+//            System.err.println("Wrong type");
+//        }
+//    }
+//
+//    @Override
+//    public int getSize() {
+//
+//    }
+//
+//    @Override
+//    public int getFreeSize() {
+//    }
+//
+//    @Override
+//    public Device getDevAtSlot(int index) {
+//        return null;
+//    }
+//
+//    @Override
+//    public boolean insertDevToSlot(Device device, int index) {
+//        return false;
+//    }
+//
+//    @Override
+//    public Device removeDevFromSlot(int index) {
+//        return null;
+//    }
+//
+//    @Override
+//    public Device getDevByIN(int in) {
+//        return null;
+//    }
+//
+//}
+//
+//    public String getType() {
+//        return type;
+//    }
 
 }
