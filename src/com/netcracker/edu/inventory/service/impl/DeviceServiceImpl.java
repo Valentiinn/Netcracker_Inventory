04bad20ca120a3ef5ea4d16116906f3e0e3737d8
Valentin Karayvansky <Valik-isuzu@mail.ru>
2016-10-24 23:33:17 +0300

    2 Task

--- src/com/netcracker/edu/inventory/service/impl/DeviceServiceImpl.java
+++ src/com/netcracker/edu/inventory/service/impl/DeviceServiceImpl.java
@@ -1,8 +1,13 @@
 package com.netcracker.edu.inventory.service.impl;
 
 import com.netcracker.edu.inventory.model.Device;
+import com.netcracker.edu.inventory.model.impl.Battery;
+import com.netcracker.edu.inventory.model.impl.Router;
+import com.netcracker.edu.inventory.model.impl.Switch;
+import com.netcracker.edu.inventory.model.impl.WifiRouter;
 import com.netcracker.edu.inventory.service.DeviceService;
 
+import java.io.*;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
@@ -25,5 +30,49 @@ class DeviceServiceImpl implements DeviceService {
             return false;
         }
         return true;
+    }
+
+    @Override
+    public void writeDevice(Device device, Writer writer) throws IOException {
+
+    }
+
+    @Override
+    public Device readDevice(Reader reader) throws IOException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void outputDevice(Device device, OutputStream outputStream) throws IOException {
+        if (outputStream == null) {
+            IllegalArgumentException e = new IllegalArgumentException("OutputStream should not be null");
+            LOGGER.log(Level.SEVERE, e.getMessage(), e);
+            throw e;
+        }
+        DataOutputStream dataOutputStream = new DataOutputStream(outputStream);
+        if (device != null) {
+            String massage = "null";
+            dataOutputStream.writeUTF(device.getClass().getName());
+            dataOutputStream.writeInt(device.getIn());
+            dataOutputStream.writeUTF(device.getType());
+            dataOutputStream.writeUTF(device.getModel() == null ? "null" : device.getModel());
+            dataOutputStream.writeUTF(device.getManufacturer() == null ? "null" : device.getManufacturer());
+            dataOutputStream.writeLong(device.getProductionDate() == null ? (long) -1 : (long) device.getProductionDate().getTime());
+        }
+    }
+
+    @Override
+    public Device inputDevice(InputStream inputStream) throws IOException, ClassNotFoundException {
+        return null;
+    }
+
+    @Override
+    public void serializeDevice(Device device, OutputStream outputStream) throws IOException {
+
+    }
+
+    @Override
+    public Device deserializeDevice(InputStream inputStream) throws IOException, ClassCastException {
+        return null;
     }
 }
